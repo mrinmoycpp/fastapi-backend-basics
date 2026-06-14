036419a33ee87d246e28f0fe7526923b5ff74ea7
D2moy <dhalimrinmoy93@gmail.com>
2026-06-14 23:47:35 +0530

    Setup FastAPI and PostgreSQL

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 
+from app.database import Base, engine
+from app.models.user import User
+from app.routers.auth import router as auth_router
+
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
+app.include_router(auth_router)
 
 @app.get("/")
-def home():
-  return {"message":"PYQ analyser"}
-
+def root():
+    return {"message": "PYQ analyser"}
